110958263f13da2f5ec4d5c3a84a2a87b17c571c
Mariano Logarzo <malogarzo@Marianos-MacBook-Pro.local>
2016-06-14 00:24:46 -0300

    update Platzigram app

--- Gulpfile.js
+++ Gulpfile.js
@@ -6,6 +6,7 @@ var browserify = require('browserify');
 var source = require('vinyl-source-stream');
 var watchify = require('watchify');
 
+//Tarea que genera app.css dentro del dir public
 gulp.task('styles', function () {
   gulp
     .src('index.scss')
@@ -14,11 +15,22 @@ gulp.task('styles', function () {
     .pipe(gulp.dest('public'));
 })
 
+//Copia el contenido de assets y genera automáticamente el directorio public
 gulp.task('assets', function () {
   gulp
     .src('assets/*')
     .pipe(gulp.dest('public'));
 })
+
+//Tarea inicial para procesar bundle de Js luego reemplazada
+//gulp.task('scripts', function () {
+//  browserify('./src/index.js')
+//  .transform(babel) //transforma usando Babel para utilizar features ES2015
+//  .bundle() //genera archivo
+//  .pipe(source('index.js')) //transforma el bundle a algo que entienda Gulp
+//  .pipe(rename('app.js'))
+//  .pipe(gulp.dest('public')); //destino del archivo
+//})
 
 function compile(watch) {
   var bundle = browserify('./src/index.js', {debug: true});
@@ -50,4 +62,5 @@ gulp.task('build', function () {
 
 gulp.task('watch', function () { return compile(true); });
 
+//Tarea default de Gulp
 gulp.task('default', ['styles', 'assets', 'build']);
--- src/index.js
+++ src/index.js
@@ -1,9 +1,24 @@
+//Aquí se encuentra todo el código del cliente
+
 require('babel-polyfill');
 var page = require('page');
+
+//Inicialmente definimos rutas
+// var main = document.getElementById('main-container')
+
+// page('/', function (ctx, next)) {
+//	 main.innerHTML = 'Home'; //diga home 	
+// )}
+
+// page('/signup', function(ctx, next)) {
+//	 main.innerHTML = 'Signup'; //diga signup
+// })
 
 require('./homepage');
 require('./signup');
 require('./signin');
 require('./footer');
 
+//Se inicializa page invocandolo sin parámetros o dandole start
+//page.start();
 page();
